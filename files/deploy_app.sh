a0fd2deaee3f39173d192fb85c48527c990d5618
y7umyp <michael_preuss@fanniemae.com>
2022-08-30 14:30:11 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to Mike's app. I want some treats now.
+  Welcome to ${PREFIX}'s app. I want some treats now.
   <!-- END -->
 
   </div>
